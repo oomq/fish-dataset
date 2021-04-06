37709e725d0a1626146a09cb005870666a569ee5
Ming <33772686+oomq@users.noreply.github.com>
2021-04-06 21:48:01 +0800

    add vedio and gt to motresult

--- json2mot.py
+++ json2mot.py
@@ -4,9 +4,9 @@ from labelme import utils
 import json
 import glob
 import numpy as np
+import cv2
 from read_json import ReadAnno
 import shutil
-
 
 root = './'
 data = './data'
@@ -45,20 +45,45 @@ def json2gt(json_path, gt1_path):
     fgt.close()
 
 
-for data_dir in os.listdir(data):
-    det_path = os.path.join(mot, data_dir, 'det')
-    gt_path = os.path.join(mot, data_dir, 'gt')
-    img_path = os.path.join(mot, data_dir, 'img1')
+def img2video(img_path, video_path):
+    i = 0
 
-    json_dir = os.path.join(data, data_dir)
-    if not (os.path.exists(det_path) or os.path.exists(gt_path) or os.path.exists(img_path)):
-        os.makedirs(det_path)
-        os.makedirs(gt_path)
-        os.makedirs(img_path)
-    json2det(json_dir, det_path)
-    json2gt(json_dir, gt_path)
-    for img in glob.glob(json_dir+'/*.jpg'):
-        shutil.copy(img, img_path)
+    for imgs in glob.glob(img_path + "/*.jpg"):
+        frame = cv2.imread(imgs)
+        if i == 0:
+            h, w, _ = frame.shape  # 获取一帧图像的宽高信息
+            fourcc = cv2.VideoWriter_fourcc(*'XVID')
+            out = cv2.VideoWriter(video_path + '/video.avi', fourcc, 25.0, (w, h), True)
+            i = 1
+        out.write(frame)  # 对视频文件写入一帧
+    #out.release()  # 释放视频流
+
+def gt2motresult(gt_path, motresult):
+    shutil.copy(gt_path)
 
 
 
+def main():
+
+    for data_dir in os.listdir(data):
+        det_path = os.path.join(mot, data_dir, 'det')
+        gt_path = os.path.join(mot, data_dir, 'gt')
+        img_path = os.path.join(mot, data_dir, 'img1')
+        video_path = os.path.join(mot, data_dir, 'video')
+
+        json_dir = os.path.join(data, data_dir)
+        if not (os.path.exists(det_path) or os.path.exists(gt_path) or os.path.exists(img_path) or os.path.exists(video_path)):
+            os.makedirs(det_path)
+            os.makedirs(gt_path)
+            os.makedirs(img_path)
+            os.makedirs(video_path)
+        json2det(json_dir, det_path)
+        json2gt(json_dir, gt_path)
+        for img in glob.glob(json_dir+'/*.jpg'):
+            shutil.copy(img, img_path)
+
+        img2video(img_path, video_path)
+
+
+if __name__ == '__main__':
+    main()
