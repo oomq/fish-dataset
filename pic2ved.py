d2e52f869d36a7146def1042f1e9e3eeb2fff276
Ming <33772686+oomq@users.noreply.github.com>
2021-04-15 11:10:03 +0800

    Add files via upload

--- pic2ved.py
+++ pic2ved.py
@@ -3,46 +3,61 @@ import glob
 import os
 
 img_path = '092'
-vedio_path = 'video'
+video_path = 'video'
 
-if not os.path.exists(vedio_path):
-    os.mkdir(vedio_path)
-
-h = 0
-w = 0
+if not os.path.exists(video_path):
+    os.mkdir(video_path)
 
 
-def img2video(img_path, video_path):
-    i = 0
+class conv:
+    def __init__(self,img_path,video_path ):
+        super().__init__()
+        self.h =0
+        self.w = 0
+        self.img_path = img_path
+        self.txt_path = img_path
+        self.video_path = video_path
+        self.gt_path = video_path
+    def img2video(self):
+        i = 0
 
-    for imgs in glob.glob(img_path + "/*.jpg"):
-        frame = cv2.imread(imgs)
-        if i == 0:
-            h, w, _ = frame.shape  # 获取一帧图像的宽高信息
-            fourcc = cv2.VideoWriter_fourcc(*'XVID')
-            out = cv2.VideoWriter(video_path + '/' + img_path + '.avi', fourcc, 25.0, (w, h), True)
-            i = 1
-        out.write(frame)  # 对视频文件写入一帧
-    # out.release()  # 释放视频流
-
-
-def txt2gt(txt_path, gt_path):
-    filenames = glob.glob(txt_path + "/*.txt")
-    f = open(gt_path + '/' + txt_path + '.txt', 'w')
-    for filename in filenames:
-        filepath = filename
-        for line in open(filepath):
-            line[0] = int(line[0]) + 1
-            line[1] *= w
-            line[3] *= w
-            line[2] *= h
-            line[4] *= h
-            str = ('{},' * 4, '{},-1,-1,-1-1').format(
-                int(filename.split('.')[0]), line[0], line[1], line[2], line[3], line[4],
-            )
-            f.writelines(str)
-    f.close()
+        for imgs in glob.glob(self.img_path + "/*.jpg"):
+            frame = cv2.imread(imgs)
+            if i == 0:
+                self.h, self.w, _ = frame.shape  # 获取一帧图像的宽高信息
+                fourcc = cv2.VideoWriter_fourcc(*'XVID')
+                out = cv2.VideoWriter(self.video_path + '/' + self.img_path + '.avi', fourcc, 25.0, (self.w, self.h), True)
+                i = 1
+            out.write(frame)  # 对视频文件写入一帧
+        # out.release()  # 释放视频流
 
 
-img2video(img_path, vedio_path)
-txt2gt(img_path, vedio_path)
+    def txt2gt(self):
+        filenames = glob.glob(self.txt_path + "/*.txt")
+        f = open(self.gt_path + '/' + self.txt_path + '.txt', 'w')
+        for filename in filenames:
+            filepath = filename
+            r =open(filepath)
+            line = r.readline()
+            while(line):
+                line = r.readline()
+                if line == '':
+                    break
+                line = line.split(' ')
+                print(line)
+                line[0] = int(line[0]) + 1
+                line[1] = float(line[1])*self.w
+                line[3] = float(line[3])*self.w
+                line[2] = float(line[2])*self.h
+                line[4] = float(line[4].replace('\n', '')) * self.h
+                frame =int(os.path.basename(filename).split('.')[0])
+                str = '{},{},{:.0f},{:.0f},{:.0f},{:.0f},-1,-1,-1-1\n'.format(
+                    frame, line[0], line[1], line[2], line[3], line[4]
+                )
+                f.writelines(str)
+        f.close()
+
+if __name__ == '__main__':
+    c = conv(img_path, video_path)
+    c.img2video()
+    c.txt2gt()
